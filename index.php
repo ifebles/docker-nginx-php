c8d858152d8a8498c7c7608d41395abdfcb4c13c
Ismael Febles <reynaldofebles@gmail.com>
2020-04-12 01:30:42 -0400

    Provide more info for the welcome page

--- index.php
+++ index.php
@@ -1,29 +1,50 @@
 <!DOCTYPE html>
 <html>
-<head>
-<title>Welcome to nginx!</title>
-<style>
-    body {
-        width: 35em;
-        margin: 0 auto;
+  <head>
+    <title>Welcome to nginx!</title>
+    <style>
+      body {
         font-family: Tahoma, Verdana, Arial, sans-serif;
-    }
-</style>
-</head>
-<body>
-<h1>Welcome to nginx!</h1>
-<p>If you see this page, the nginx web server is successfully installed and
-working. Further configuration is required.</p>
+      }
 
-<p>For online documentation and support please refer to
-<a href="http://nginx.org/">nginx.org</a>.<br/>
-Commercial support is available at
-<a href="http://nginx.com/">nginx.com</a>.</p>
+      div {
+        margin: 0 auto;
+      }
+    </style>
+  </head>
 
-<p><em>Thank you for using nginx.</em></p>
+  <body>
+    <div style="max-width: 35em">
+      <h1>Welcome to nginx!</h1>
+      <p>
+        If you see this page, the nginx web server is successfully installed and
+        working. Further configuration is required.
+      </p>
 
-<br />
-<h3>PHP validation:</h3>
-<pre><?php print_r($_SERVER) ?></pre>
-</body>
+      <p>
+        For online documentation and support please refer to
+        <a href="http://nginx.org/">nginx.org</a>.<br/>
+        Commercial support is available at
+        <a href="http://nginx.com/">nginx.com</a>.
+      </p>
+
+      <p><em>Thank you for using nginx.</em></p>
+    </div>
+
+    <br />
+
+    <div style="max-width: 55em">
+      <h3>PHP validation:</h3>
+      <pre style="white-space: pre-wrap;overflow-wrap: break-word;">
+        <?php print_r($_SERVER) ?>
+      </pre>
+    </div>
+
+    <br />
+
+    <div>
+      <h3 class="center">PHP info</h3>
+      <?php phpinfo() ?>
+    </div>
+  </body>
 </html>
